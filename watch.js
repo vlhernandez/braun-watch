37acc51eac58aac477824420b35a55311f03dfed
Veronica Hernandez <vlhernandez@gmail.com>
2016-06-13 07:15:53 -0500

    found some help on plotting around a circle

--- watch.js
+++ watch.js
@@ -19,25 +19,28 @@ function placeTicks(){
     var liTop = Number(Math.round(circleCenterY + radius * Math.sin(angle)));
     element.style.left = liLeft+"px";
     element.style.top = liTop+"px";
-    angle += step;
+    angle = angle - step;
   }
 }
 
 function placeNumbers(){
+  var circle = document.querySelector(".number-wrapper")
   var listElements = document.querySelectorAll("ul.o-clock > li");
-  var step = (2 * Math.PI)/listElements.length;
-  var angle = 0;
-  var circleCenterX = 205;
-  var circleCenterY = 180;
-  var radius = 115 / 2*Math.PI;
+  var len = listElements.length;
+  var diam = parseInt( window.getComputedStyle(circle).getPropertyValue('width'));
+  var radius = diam / 2;
+  var imgW = listElements[0].getBoundingClientRect().width;
+  var radius2 = radius - imgW;
+  var angle = Math.PI / 2;
+  var corner = 2 * Math.PI / len;
 
-  for (var i = 0; i < listElements.length; i++) {
+
+  for (var i = 0; i < len ; i++) {
     var element = listElements[i];
-    var liLeft = Number(Math.round(circleCenterX + radius * Math.cos(angle)));
-    var liTop = Number(Math.round(circleCenterY + radius * Math.sin(angle)));
-    element.style.left = liLeft+"px";
-    element.style.top = liTop+"px";
-    angle += step;
+    element.style.left = parseInt( (radius - imgW / 2) +  (radius2 * Math.cos(angle)))+'px';
+    element.style.top = parseInt( (radius - imgW / 2) -  (radius2 * Math.sin(angle)))+'px';
+
+    angle = angle - corner;
   }
 }
 
